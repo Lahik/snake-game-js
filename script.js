31c7d546cb2373da0605156c883e7bfb7f10bdc0
Lahik <lahik65@gmail.com>
2025-04-08 22:56:11 +0530

    Randomize Placing Food

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 let board;
-const unitSize = 25;
-const rows = 20;
-const cols = 20;
+const UNIT_SIZE = 25;
+const ROWS = 20;
+const COLS = 20;
 let context;
 
-let snakeX = unitSize * 5;
-let snakeY = unitSize * 5;
+let snakeX = UNIT_SIZE * 5;
+let snakeY = UNIT_SIZE * 5;
 
-var foodX = unitSize * 10;
-var foodY = unitSize * 10;
+var foodX;
+var foodY;
 
 window.onload = () => {
     board = document.getElementById("board");
-    board.height = cols * unitSize;
-    board.width = rows * unitSize;
+    board.height = COLS * UNIT_SIZE;
+    board.width = ROWS * UNIT_SIZE;
     context = board.getContext("2d");
     
+    placeFood();
     update();
 }
 
@@ -24,8 +25,13 @@ function update() {
     context.fillRect(0, 0, board.width, board.height);
 
     context.fillStyle = "lime";
-    context.fillRect(snakeX, snakeY, unitSize, unitSize);
+    context.fillRect(snakeX, snakeY, UNIT_SIZE, UNIT_SIZE);
 
     context.fillStyle = "red";
-    context.fillRect(foodX, foodY, unitSize, unitSize);
+    context.fillRect(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
+}
+
+function placeFood() {
+    foodX = Math.floor(Math.random() * ROWS) * UNIT_SIZE;
+    foodY = Math.floor(Math.random() * COLS) * UNIT_SIZE;
 }
