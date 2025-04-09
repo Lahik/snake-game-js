d61fc67ebc5a66995d3eeb23a3570fb0101a8c6d
Lahik <lahik65@gmail.com>
2025-04-09 14:45:30 +0530

    Snake Movement

--- script.js
+++ script.js
@@ -7,8 +7,11 @@ let context;
 let snakeX = UNIT_SIZE * 5;
 let snakeY = UNIT_SIZE * 5;
 
-var foodX;
-var foodY;
+let foodX;
+let foodY;
+
+let velocityX = 0;
+let velocityY = 0;
 
 window.onload = () => {
     board = document.getElementById("board");
@@ -17,18 +20,44 @@ window.onload = () => {
     context = board.getContext("2d");
     
     placeFood();
-    update();
+
+    document.addEventListener("keydown", changeDirection);
+
+    setInterval(update, 100);
 }
 
 function update() {
     context.fillStyle = "black";
     context.fillRect(0, 0, board.width, board.height);
-
-    context.fillStyle = "lime";
-    context.fillRect(snakeX, snakeY, UNIT_SIZE, UNIT_SIZE);
-
+    
     context.fillStyle = "red";
     context.fillRect(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
+    
+    context.fillStyle = "lime";
+    snakeX += (velocityX * UNIT_SIZE);
+    snakeY += (velocityY * UNIT_SIZE);
+    context.fillRect(snakeX, snakeY, UNIT_SIZE, UNIT_SIZE);
+}
+
+function changeDirection(e) {
+    switch(e.code) {
+        case 'ArrowUp':
+            velocityX = 0;
+            velocityY = -1;       
+            break;
+        case 'ArrowDown':
+            velocityX = 0;
+            velocityY = 1;       
+            break;
+        case 'ArrowLeft':
+            velocityX = -1;
+            velocityY = 0;       
+            break;
+        case 'ArrowRight':
+            velocityX = 1;
+            velocityY = 0;       
+            break;
+    }
 }
 
 function placeFood() {
